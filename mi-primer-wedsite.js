7705ef7dec8a6cb3727f13c9e9e5419dcef35e85
JLaspiur <134982358+JLaspiur@users.noreply.github.com>
2023-05-29 22:40:04 -0300

    Initial commit

---
+++ mi-primer-wedsite.js
@@ -0,0 +1,7 @@
+function showAlert() { alert('por favor, presta atencion'); }
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+
